c7edbaf83896aa1aa6291c100835cda6a575940e
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-10-10 04:21:56 -0700

    updated

--- Currency_Mega_Project/src/App.jsx
+++ Currency_Mega_Project/src/App.jsx
@@ -1,19 +1,83 @@
 import { useState } from "react";
-import "./index.css";
+import Input from "./components/Input";
+import CustomHook from "./hook/CustomHook";
+
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(83.1158414);
+
+  const currencyInfo = CustomHook(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount);
+   setAmount(convertedAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
   return (
-    <>
-      <div
-        className="flex justify-center text-red-500 w-full items-center h-screen opacity-75 bg-cover bg-no-repeat"
-        style={{
-          backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
-        }}
-      >
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+      }}
+    >
+      <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
-          SHabbir
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full mb-2">
+             
+              <Input
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                oncurrencychange={(currency) => setFrom(currency)}
+                Scurrency={from}
+                onamountchange={(amount) => setAmount(amount)}
+              />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                onClick={swap}
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <Input
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                oncurrencychange={(currency) => setTo(currency)}
+                Scurrency={to}
+                Disabled
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert {from.toUpperCase()} to {to.toUpperCase()}
+            </button>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ Currency_Mega_Project/src/components/Input.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function Input({
+  label,
+  amount,
+  onamountchange,
+  oncurrencychange,
+  Scurrency = "usd",
+  currencyOptions = [],
+  Disabled
+}) {
+  return (
+    <div className="bg-white p-3 rounded-lg text-sm flex">
+      <div className="w-1/2">
+        <label htmlFor="price">{label}</label>
+        <input
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          id="price"
+          placeholder="amount"
+         
+          value={amount}
+          onChange={(e) =>
+            onamountchange && onamountchange(Number(e.target.value))
+          }
+          disabled={Disabled}
+        />
+      </div>
+      <div className="w-1/2 justify-end text-right ">
+       <div> <label htmlFor="CurrencyType">CurrencyType</label></div>
+        <select
+          id="CurrencyType"
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          value={Scurrency}
+          onChange={(e) => oncurrencychange(e.target.value)}
+         
+        >
+          {currencyOptions.map((currency) => (
+            <option value={currency}>{currency}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
