eaba8e3e5be5bcf292058b5a97ae00358aebb6a2
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-10-18 06:52:14 -0700

    mai

--- Theme_Provide_Context/src/App.jsx
+++ Theme_Provide_Context/src/App.jsx
@@ -1,29 +1,6 @@
 
-import { useEffect, useState } from 'react'
-import './App.css'
-import { ThemeProvider } from './contexts/theme'
-import ThemeBtn from './components/ThemeBtn'
-import Card from './components/Card'
-
 function App() {
-  const [themeMode, setThemeMode] = useState("light")
-
-  const lightTheme = () => {
-    setThemeMode("light")
-  }
-
-  const darkTheme = () => {
-    setThemeMode("dark")
-  }
-
-  // actual change in theme
-
-  useEffect(() => {
-    document.querySelector('html').classList.remove("light", "dark")
-    document.querySelector('html').classList.add(themeMode)
-  }, [themeMode])
-  
-
+ 
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
