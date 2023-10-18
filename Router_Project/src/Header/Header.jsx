3d6d0b7422f6f738d173343ac766abc71839d67f
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-10-18 01:25:35 -0700

    df

--- Router_Project/src/Header/Header.jsx
+++ Router_Project/src/Header/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import {Link, NavLink, Navigate, useNavigate} from 'react-router-dom'
 
 export default function Header() {
+    const navigate=useNavigate()
+    const Navigater=()=>{
+        navigate('/')
+    }
     return (
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
@@ -16,6 +20,7 @@ export default function Header() {
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="#"
+                            onClick={Navigater}
                             className="text-gray-800 hover:bg-gray-50   font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
