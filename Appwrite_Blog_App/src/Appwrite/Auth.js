ed7835bcbe509400180a2a697bdd5eb276b2de8c
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-10-25 05:33:10 -0700

    s

--- Appwrite_Blog_App/src/Appwrite/Auth.js
+++ Appwrite_Blog_App/src/Appwrite/Auth.js
@@ -4,7 +4,36 @@ export class Authservice{
     client= new Client();
     account;
     constructor(){
-        this.client.setEndpoint(Config.appwrite_Url).setProject(Config.Project_id)
+        this.client.setEndpoint(Config.appwrite_Url).setProject(Config.Project_id);
+        this.account= new Account(this.client)
+    }
+    async createAccount({email,password,name}){
+        try {
+            const useraccount= await this.account.create(ID.unique(), email, name, password)
+            if(useraccount){
+                return this.login({email, password})
+            }
+            else{
+                return useraccount
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+    async login({email, password}){
+        try {
+            return await this.account.createEmailSession(email,password)
+        } catch (error) {
+            throw error;
+            
+        }
+    }
+    async currentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            throw error
+        }
     }
 };
 const authservice = new Authservice()
