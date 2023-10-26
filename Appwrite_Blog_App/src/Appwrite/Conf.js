ab5b51b468826680f66955227b31b504c0796f6b
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-10-26 06:57:34 -0700

    d

--- Appwrite_Blog_App/src/Appwrite/Conf.js
+++ Appwrite_Blog_App/src/Appwrite/Conf.js
@@ -1,4 +1,4 @@
-import { Client } from "appwrite";
+import { Client, Databases, Query, Storage } from "appwrite";
 import Config from "../Configure/config";
 export class Service {
     client = new Client();
@@ -9,7 +9,44 @@ export class Service {
         this.databases = new Databases(this.client)
         this.bucket = new Storage(this.client)
     }
-    async createpost({title,slug, content, featuredimage, status, userId })
+    async createpost({title,slug, content, featuredimage, status, userId }){
+        try {
+            return await this.databases.createDocument(Config.Database_id, Config.Collection_id, slug, {title, content, featuredimage, status, userId})
+        } catch (error) {
+            throw error
+        }
+    }
+    async updatepost(slug,{title, content, featuredimage, status}){
+        try {
+            return await this.databases.updateDocument(Config.Database_id, Config.Collection_id, slug, {title, content, featuredimage, status})
+        } catch (error) {
+            throw error
+        }
+    }
+    async deletepost(slug){
+        try {
+            await this.databases.deleteDocument(Config.Database_id, Config.Collection_id, slug)
+            return true
+        } catch (error) {
+            return false
+        }
+    }
+    async getpost(slug){
+        try {
+            return await this.databases.getDocument(Config.Database_id, Config.Collection_id, slug)
+            
+        } catch (error) {
+            return false
+            
+        }
+    }
+    async allpost(queries= Query('status', 'active')){
+        try {
+            return await this.databases.listDocuments(Config.Database_id, Config.Collection_id, queries)
+        } catch (error) {
+            return false
+        }
+    }
 }
 const service = new Service()
 export default service
