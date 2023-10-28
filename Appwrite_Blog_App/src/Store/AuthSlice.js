dabf2d1bcf2177b1b9adef63bdd30b6b17fb1ac5
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-10-28 02:49:57 -0700

    ss

--- Appwrite_Blog_App/src/Store/AuthSlice.js
+++ Appwrite_Blog_App/src/Store/AuthSlice.js
@@ -7,8 +7,16 @@ const AuthSlice= createSlice({
     name: 'auth',
     initialstate,
     reducers:{
-        login: (state, action)=>{}
+        login: (state, action)=>{
+            state.status=true;
+            state.userData= action.paylaod
+        },
+        login: (state)=>{
+            state.status=false;
+            state.userData= null
+        },
     }
+
     
 })
 export default AuthSlice.reducers
