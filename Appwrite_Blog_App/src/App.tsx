4b32efb2fd9db6a928ba830e8464b919a4123915
Shabbir-Anjum <mahrshabbir768@gmail.com>
2023-11-03 03:24:14 -0700

    ss

--- Appwrite_Blog_App/src/App.tsx
+++ Appwrite_Blog_App/src/App.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react"
 import { useDispatch } from "react-redux"
 import authservice from "./Appwrite/Auth"
-
+import {login, logout} from "./Store/AuthSlice"
 function App() {
 const [loading, setloading]= useState(true);
 const dispatch= useDispatch()
 useEffect(()=>{
-  authservice.currentUser()
+  authservice.currentUser().then((userdata)=> dispatch(login({userdata})))
 },[])
   return (
     <>
