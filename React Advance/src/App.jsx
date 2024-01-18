957aa721e3a0b17b1d1375bc17571d879af87636
Shabbir-Anjum <mahrshabbir768@gmail.com>
2024-01-18 20:40:08 +0500

    fdf

--- React Advance/src/App.jsx
+++ React Advance/src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
 import Todo from './Todo'
 
@@ -13,27 +13,48 @@ function App() {
 // function chnageC(){
 //   setcolors('green')
 // }
-const [length, setlength]= useState(9)
-const [text, settext]= useState(' ')
-const passwordG=()=>{
-let str='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-let char = Math.floor(Math.random()*str + 1)
-settext(char)
-}
-useEffect(
- ()=>{passwordG()}
-  ,[])
+// const [length, setlength]= useState(2)
+// const [data, setdata]= useState(true)
+// const [text, settext]= useState(' ')
+// const refe= useRef()
+// const passwordG= useCallback(()=>{
+//   let str='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+//   let pass= ''
+//   if(data){
+//    str=str + '!@#$%^&*()'
+//   }
+
+
+// let leng= str.length;
+
+// for(let i =1 ; i<=length; i++)
+// {
+//   let char = Math.floor(Math.random()*leng + 1)
+//   pass= pass + str.charAt(char);
+// }
+// settext(pass)
+// refe.current?.select();
+// window.navigator.clipboard.writeText(pass)
+// },[length, text, data]) 
+// useEffect(
+//  ()=>{passwordG()}
+//   ,[length])
+
+let t='text-yellow-700';
   return (
-    <div>
+    <div className={` ${t} h-screen`}>
     {/* <Todo color={chnageC}/> */}
       {/* <p>{value}</p>
       <button onClick={valuechange}>Update</button> */}
-      <input type="text" value={text} onChange={(e)=>{settext(e.target.value)}} readOnly />
-     <input value={length} type="range" max={15} min={8} onChange={(e)=>{setlength(e.target.value)}}/>
+      {/* <input type="text" ref={refe} value={text} onChange={(e)=>{settext(e.target.value)}} readOnly />
+     <input value={length} type="range" max={15} min={1} onChange={(e)=>{setlength(e.target.value)}}/>
      <p>
       length={length}
       text={text}
      </p>
+     <input type="checkbox" onChange={()=>{setdata((prev)=> !prev)} }/> */}
+hgjghjghjg
+
     </div>
   )
 }
--- React Advance/src/Todo.jsx
+++ React Advance/src/Todo.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Todo(props) {
   return (
     <div className={`max-w-full min-h-screen border-2 border-cyan-700 `}>
-      display{props.color}
-      <button onClick={props.color}>button</button>
+      {/* display{props.color}
+      <button onClick={props.color}>button</button> */}
     </div>
   )
 }
